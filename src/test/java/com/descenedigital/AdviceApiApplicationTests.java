3379fe80edfe51275219786481c466356b823143
fhadk <fahad.khan1991@gmail.com>
2025-08-08 21:38:48 +0500

    init /src

---
+++ src/test/java/com/descenedigital/AdviceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.descenedigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdviceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
